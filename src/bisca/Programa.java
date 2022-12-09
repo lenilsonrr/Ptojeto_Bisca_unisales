53edc41d3889b9114d4e387fbb2af4d73c9b4d7f
Lenilson <lenilsonrr86@gmail.com>
2022-12-09 19:33:06 -0300

    acertos

--- src/bisca/Programa.java
+++ src/bisca/Programa.java
@@ -3,11 +3,6 @@ package bisca;
 public class Programa {
 
 	public static void main(String[] args) {
-		Carta cartaAs = new Carta ("As", "Copas", 11);
-		System.out.println(cartaAs);
-		
-		Baralho baralho1 = new Baralho();
-		System.out.println("___________teste___________");
-		System.out.println(baralho1);
+		System.out.println("oi");
 	}
 }
