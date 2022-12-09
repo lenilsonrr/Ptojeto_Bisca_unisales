f43b0acf07785c24eeb93128506fd98d5f1323bd
juancso <juancso69@gmail.com>
2022-12-09 09:46:43 -0300

    Criando classe jogador

--- src/bisca/Jogador.java
+++ src/bisca/Jogador.java
@@ -1,5 +1,61 @@
 package bisca;
 
+
 public class Jogador {
+	private String nome;
+	private Integer pontos;
+	private List<Carta> cartasJogador;
+	
+	
+	public Jogador(String nome, int pontos,List<Carta> cartas) {
+		this.nome = nome;
+		this.pontos = pontos;
+		this.cartasJogador = cartas;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public Integer getPontos() {
+		return pontos;
+	}
+
+
+	public void setPontos(Integer pontos) {
+		this.pontos = pontos;
+	}
+
+
+	public List<Carta> getCartasJogador() {
+		return cartasJogador;
+	}
+
+
+	public void setCartasJogador(List<Carta> cartasJogador) {
+		this.cartasJogador = new ArrayList<>();
+	}
+	
+	public void somarPontos(int pontos) {
+		this.pontos += pontos;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		int cont = 0;
+		for (Carta c : cartasJogador) {
+			cont++;
+			sb.append("("+cont+")  "+c);
+		}
+		return nome +", pontos: "+ pontos + "\n\n Nª  Suas cartas \n\n" + sb.toString();
+	}
 
 }
