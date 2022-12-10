12a900a8adcd3ad2dcc96fc643f441e4c7622b46
Kaique RFS <kaiquerfs53@gmail.com>
2022-12-10 17:42:24 -0300

    adicionando sortear o trunfo ao Baralho

--- src/bisca/Baralho.java
+++ src/bisca/Baralho.java
@@ -1,13 +1,15 @@
 package bisca;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 public class Baralho {
 
 	private List<Carta> cartas = new ArrayList<>();
-	
-	//Criando baralho //
+
+	// Criando baralho //
 
 	public Baralho() {
 		String[] faces = { "As", "2", "3", "4", "5", "6", "7", "Q", "J", "K" };
@@ -17,6 +19,80 @@ public class Baralho {
 			this.cartas.add(new Carta(faces[i], "Ouros", 0));
 			this.cartas.add(new Carta(faces[i], "Paus", 0));
 		}
+	}
+
+	public List<Carta> getCartas() {
+		return cartas;
+	}
+
+	public void setCartas(List<Carta> cartas) {
+		this.cartas = new ArrayList<>();
+	}
+
+	public void adicionadoValorAsCartas() {
+		for (int i = 0; i < cartas.size(); i++) {
+			if (getCartas().get(i).getFaces() == ("As")) {
+				getCartas().get(i).setValor(11);
+				getCartas().get(i).setPeso(10);
+				;
+			}
+			if (getCartas().get(i).getFaces() == "7") {
+				getCartas().get(i).setValor(10);
+				getCartas().get(i).setPeso(9);
+			}
+			if (getCartas().get(i).getFaces() == "K") {
+				getCartas().get(i).setValor(4);
+				getCartas().get(i).setPeso(8);
+			}
+			if (getCartas().get(i).getFaces() == "J") {
+				getCartas().get(i).setValor(3);
+				getCartas().get(i).setPeso(7);
+			}
+			if (getCartas().get(i).getFaces() == "Q") {
+				getCartas().get(i).setValor(2);
+				getCartas().get(i).setPeso(6);
+			}
+			if (getCartas().get(i).getFaces() == "6") {
+				getCartas().get(i).setPeso(5);
+			}
+			if (getCartas().get(i).getFaces() == "5") {
+				getCartas().get(i).setPeso(4);
+			}
+			if (getCartas().get(i).getFaces() == "4") {
+				getCartas().get(i).setPeso(3);
+			}
+			if (getCartas().get(i).getFaces() == "3") {
+				getCartas().get(i).setPeso(2);
+			}
+			if (getCartas().get(i).getFaces() == "2") {
+				getCartas().get(i).setPeso(1);
+			}
+		}
+	}
+
+	public void embaralhar() {
+		Collections.shuffle(cartas);
+	}
+
+	private Random rand = new Random();
+
+	public Carta cartaParaTrunfo() {
+		int index = rand.nextInt(cartas.size() - 1);
+		Carta cartaTrunfo = cartas.get(index);
+		Collections.swap(cartas, index, cartas.size() - 1);
+		if (cartaTrunfo.getFaces() == "7" || cartaTrunfo.getFaces() == "As") {
+			index = rand.nextInt(cartas.size() - 1);
+			cartaTrunfo = cartas.get(index);
+			Collections.swap(cartas, index, cartas.size() - 1);
+		}
+		return cartaTrunfo;
+	}
+
+	public Carta comprarUmaCarta() {
+
+		Carta retirandoCartas = cartas.get(0);
+		cartas.remove(0);
+		return retirandoCartas;
 	}
 
 	@Override
--- src/bisca/Programa.java
+++ src/bisca/Programa.java
@@ -6,34 +6,36 @@ import java.util.List;
 public class Programa {
 
 	public static void main(String[] args) {
-		
-		//testando carta//
-		Carta cartaAs = new Carta ("As", "Copas", 11);
-		Carta cart7 = new Carta ("7", "Copas", 11);
-		Carta cartaK = new Carta ("K", "Copas", 11);
+
+		// testando carta//
+		Carta cartaAs = new Carta("As", "Copas", 11);
+		Carta cart7 = new Carta("7", "Copas", 11);
+		Carta cartaK = new Carta("K", "Copas", 11);
 		System.out.println(cartaAs);
-		
-		//testando baralho//
+
+		// testando baralho//
 		Baralho baralho1 = new Baralho();
-		System.out.println("___________teste___________");
-		 baralho1.embaralhar();
+		System.out.println("____teste____");
+		baralho1.embaralhar();
+		baralho1.cartaParaTrunfo();
+		System.out.println("carta trunfo:" + baralho1.cartaParaTrunfo() + "\n");
 		System.out.println(baralho1);
-		
-		//testando Jogador//
+
+		// testando Jogador//
 		List<Carta> cartasJogador = new ArrayList<>();
 		cartasJogador.add(cartaAs);
 		cartasJogador.add(cartaK);
 		cartasJogador.add(cart7);
-		Jogador jogadorTeste = new Jogador("Jog1", 0, cartasJogador) ;
-		Jogador jogadorTeste2 = new Jogador("Jog2", 0, cartasJogador) ;
+		Jogador jogadorTeste = new Jogador("Jog1", 0, cartasJogador);
+		Jogador jogadorTeste2 = new Jogador("Jog2", 0, cartasJogador);
 		System.out.println(jogadorTeste);
-		
-		//testando partida//
+
+		// testando partida//
 		Partida partidaTeste = new Partida(baralho1);
 		partidaTeste.setCartaNipe(cartaK);
 		partidaTeste.setjogador1(jogadorTeste);
 		partidaTeste.setjogador2(jogadorTeste2);
 		System.out.println(partidaTeste);
-		
+
 	}
 }
